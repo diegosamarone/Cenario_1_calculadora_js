84c7f43efa8239714931fb403551d11ac017b1b1
diegosamarone <diegobict@gmail.com>
2023-11-16 02:44:05 -0300

    Implementação das funções insert(), clean(), back() e uma função adicional update_console()

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,28 @@
+let numeros_digitados = []; //array armazena os numeros digitados para a função back/clean
 
 function insert(num){
-    
+    numeros_digitados.push(num);
+    update_console();    
 }
 function clean(){
-    
+    numeros_digitados = [];
+    console.clear();   
 }
 function back(){
-    
+    if(numeros_digitados.length > 0){
+        numeros_digitados.pop(); // método pop para remover o último elemento do array
+        update_console();
+    }
 }
 function calcular(){
     
 }
 
 module.exports = {insert, clean, back, calcular}
+
+
+// Função adicional para atualizar os números no console 
+function update_console() {
+    console.clear(); // Limpar o console do navegador
+    numeros_digitados.forEach(numero => console.log(numero)); //mostra os números digitados 
+}
